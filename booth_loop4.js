f3bf714391c5a165b52995c5000e164b6f266107
Benjamin Lichtman <runite623@gmail.com>
2016-04-13 21:28:44 -0400

    commented code

--- booth_loop4.js
+++ booth_loop4.js
@@ -1,12 +1,16 @@
 var Sound = require('node-mpg123');
 var memwatch = require('memwatch-next');
 
+// print to console if memory leak detected
 memwatch.on('leak', function(info) { console.log(info); });
 
-// fire and forget:
+// initialize sound
 var sound = new Sound('bg.mov');
 
+// play for the first time
 sound.play();
 setInterval(function(){
+  // repeat song every 3 mins 22.508 seconds
+  // clip is 3 mins 22.507 seconds  long
   sound.play();
 }, 202508);
